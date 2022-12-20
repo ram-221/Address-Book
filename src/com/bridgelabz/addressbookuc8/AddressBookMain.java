fa6755cab51a844c83456334aae75282daadbf65
ram-221 <rameshmalli857.com>
2022-12-20 12:06:58 +0530

    Added: Contacts Search By City Or State

---
+++ src/com/bridgelabz/addressbookuc8/AddressBookMain.java
@@ -0,0 +1,52 @@
+package com.bridgelabz.addressbookuc8;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+	static Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		System.out.println("\n***Welcome To Address Book Program***\n");
+
+		AddressBook details = new AddressBook();
+
+		int choice;
+		do {
+			System.out.println("1.Add new address book\n2.Add contact\n3.Show Contact\n4.Edit Contact\n5.Delete Contact"
+					+ "\n6.Select address book\n7.searchContact\n8.Exit\n");
+			System.out.println("Enter your choice: ");
+			choice = input.nextInt();
+			switch (choice) {
+			case 1:
+				details.addNewAddressBook();
+				break;
+			case 2:
+				Contact Contact = details.getContactInput();
+				details.addContact(Contact);	
+				break;
+			case 3:
+				details.viewContacts();
+				break;
+			case 4:
+				details.editContact();
+				break;
+			case 5:
+				details.deleteContact();
+				break;
+			case 6:
+				details.selectAddressBook();
+				break;
+			case 7:
+				details.searchContact();
+				break;
+			case 8:
+				System.out.println("Program Terminate Successfully!!!");
+				break;
+			default:
+				System.out.println("Enter Valid Input...");
+			}
+		} while (choice != 8);
+	}
+}
